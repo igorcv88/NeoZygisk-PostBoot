2574e6f88665e9f2a6d7b5530aaa3850d1f6c630
Igor <igorv88@gmail.com>
2026-07-30 08:19:02 -0300

    feat: re-run idempotent bootstrap from late service stage

--- module/src/service.sh
+++ module/src/service.sh
@@ -7,17 +7,19 @@ if [ "$ZYGISK_ENABLED" ]; then
   exit 0
 fi
 
-cd "$MODDIR"
+cd "$MODDIR" || exit 1
 
 if [ "$(which magisk)" ]; then
   for file in ../*; do
     if [ -d "$file" ] && [ -d "$file/zygisk" ] && ! [ -f "$file/disable" ]; then
       if [ -f "$file/service.sh" ]; then
-        cd "$file"
+        cd "$file" || continue
         log -p i -t "zygisk-sh" "Manually trigger service.sh for $file"
         sh "$(realpath ./service.sh)" &
-        cd "$MODDIR"
+        cd "$MODDIR" || exit 1
       fi
     fi
   done
 fi
+
+/system/bin/sh "$MODDIR/postboot-bootstrap.sh" ensure
