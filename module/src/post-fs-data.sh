0f186212b68373288e783909e3317c4f24188215
Igor <igorv88@gmail.com>
2026-07-30 08:18:46 -0300

    refactor: route post-fs-data through idempotent bootstrap

--- module/src/post-fs-data.sh
+++ module/src/post-fs-data.sh
@@ -5,51 +5,19 @@ if [ "$ZYGISK_ENABLED" ]; then
   exit 0
 fi
 
-cd "$MODDIR"
+cd "$MODDIR" || exit 1
 
 if [ "$(which magisk)" ]; then
   for file in ../*; do
     if [ -d "$file" ] && [ -d "$file/zygisk" ] && ! [ -f "$file/disable" ]; then
       if [ -f "$file/post-fs-data.sh" ]; then
-        cd "$file"
+        cd "$file" || continue
         log -p i -t "zygisk-sh" "Manually trigger post-fs-data.sh for $file"
         sh "$(realpath ./post-fs-data.sh)"
-        cd "$MODDIR"
+        cd "$MODDIR" || exit 1
       fi
     fi
   done
 fi
 
-create_sys_perm() {
-  mkdir -p $1
-  chmod 555 $1
-  chcon u:object_r:system_file:s0 $1
-}
-
-TMP_PATH=@WORK_DIRECTORY@
-
-if [ -d $TMP_PATH ]; then
-  rm -rf $TMP_PATH
-fi
-
-create_sys_perm $TMP_PATH
-
-if [ -f $MODDIR/lib64/libzygisk.so ];then
-  create_sys_perm $TMP_PATH/lib64
-  cp $MODDIR/lib64/libzygisk.so $TMP_PATH/lib64/libzygisk.so
-  chcon u:object_r:system_file:s0 $TMP_PATH/lib64/libzygisk.so
-fi
-
-if [ -f $MODDIR/lib/libzygisk.so ];then
-  create_sys_perm $TMP_PATH/lib
-  cp $MODDIR/lib/libzygisk.so $TMP_PATH/lib/libzygisk.so
-  chcon u:object_r:system_file:s0 $TMP_PATH/lib/libzygisk.so
-fi
-
-[ "$DEBUG" = true ] && export RUST_BACKTRACE=1
-
-if [ -f $MODDIR/bin/zygisk-ptrace64 ];then
-$MODDIR/bin/zygisk-ptrace64 monitor &
-elif [ -f $MODDIR/bin/zygisk-ptrace32 ];then
-$MODDIR/bin/zygisk-ptrace32 monitor &
-fi
+exec /system/bin/sh "$MODDIR/postboot-bootstrap.sh" ensure
