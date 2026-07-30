92327c306eec08c9a022d9302f640bbb781d9faa
Igor <igorv88@gmail.com>
2026-07-30 08:23:04 -0300

    chore: clean phase 2 runtime state on uninstall

--- module/src/uninstall.sh
+++ module/src/uninstall.sh
@@ -1,5 +1,8 @@
 #!/system/bin/sh
 
-export TMP_PATH=@WORK_DIRECTORY@
+WORK=@WORK_DIRECTORY@
 
-rm -rf $TMP_PATH
+rm -rf "$WORK"
+rm -rf /data/local/tmp/neozygisk-postboot.lock
+rm -f /data/local/tmp/neozygisk-postboot.status
+rm -f /data/local/tmp/neozygisk-postboot.log
